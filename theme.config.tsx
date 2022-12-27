80820fa780ece4c62a5883f1091a0ba087eb4eb9
Allyedge <alimarslank@gmail.com>
2022-12-27 12:12:01 +0100

    Update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>HPL Docs</span>,
@@ -7,7 +8,51 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/Allyedge/hpl-docs",
   footer: {
-    text: "HPL Documentation",
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://hpl-docs.vercel.app/" target="_blank">
+          HPL Docs
+        </a>
+        .
+      </span>
+    ),
+  },
+  navigation: {
+    prev: true,
+    next: true,
+  },
+  useNextSeoProps() {
+    const { route } = useRouter();
+
+    if (route !== "/") {
+      return {
+        titleTemplate: "%s - HPL Docs",
+      };
+    }
+
+    return {
+      titleTemplate: "HPL Docs",
+    };
+  },
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:url"
+          content={`https://hpl-docs.vercel.app${asPath}`}
+        />
+        <meta property="og:title" content={frontMatter.title || "HPL Docs"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "Documentation for HPL."}
+        />
+      </>
+    );
   },
 };
 
